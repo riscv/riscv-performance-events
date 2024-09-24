61a52c693d19b400440b3699dd2ee2c94063c542
Dmitriy Ryabtsev <d.ryabtsev@yadro.com>
2024-09-24 16:39:40 +0300

    Address feedback from group discussion
    Signed-off-by: Dmitriy Ryabtsev <d.ryabtsev@yadro.com>

--- gen_event_tables_adocs.py
+++ gen_event_tables_adocs.py
@@ -8,36 +8,34 @@ event_files = ['cache_retired.json', 'cache_spec.json', 'prediction_retired.json
 
 metric_files = ['cache_retired_metrics.json', 'cache_spec_metrics.json',
                 'prediction_metrics.json', 'rvv_retired_metrics.json', 'rvv_spec_metrics.json',
-                'tlb_retired_metrics.json', 'tlb_spec_metrics.json', 'topdown_metrics.json',
-                'general_metrics.json']
+                'tlb_retired_metrics.json', 'tlb_spec_metrics.json', 'topdown_metrics.json']
 
-file_to_group = {'cache_retired.json': 'CACHE_RETIRED',
-                 'cache_spec.json': 'CACHE_SPEC',
-                 'prediction_retired.json': 'PRD_RETIRED',
-                 'prediction_spec.json': 'PRD_SPEC',
-                 'rvv_retired.json': 'RVV_RETIRED',
-                 'rvv_spec.json': 'RVV_SPEC',
-                 'tlb_retired.json': 'TLB_RETIRED',
-                 'tlb_spec.json': 'TLB_SPEC',
+file_to_group = {'cache_retired.json': 'CACHE',
+                 'cache_spec.json': 'CACHE',
+                 'prediction_retired.json': 'CTRL_FLOW',
+                 'prediction_spec.json': 'CTRL_FLOW',
+                 'rvv_retired.json': 'RVV',
+                 'rvv_spec.json': 'RVV',
+                 'tlb_retired.json': 'TLB',
+                 'tlb_spec.json': 'TLB',
                  'topdown.json': 'TOPDOWN',
                  'general.json': 'GEN',
                  'retired.json': 'RETIRED',
                  'spec.json': 'SPEC',
-                 'cache_retired_metrics.json': 'CACHE_RETIRED',
-                 'cache_spec_metrics.json': 'CACHE_SPEC',
-                 'prediction_metrics.json': 'PRD_RETIRED',
-                 'rvv_retired_metrics.json': 'RVV_RETIRED',
-                 'rvv_spec_metrics.json': 'RVV_SPEC',
-                 'tlb_retired_metrics.json': 'TLB_RETIRED',
-                 'tlb_spec_metrics.json': 'TLB_SPEC',
-                 'topdown_metrics.json': 'TOPDOWN',
-                 'general_metrics.json': 'GEN'
+                 'cache_retired_metrics.json': 'CACHE',
+                 'cache_spec_metrics.json': 'CACHE',
+                 'prediction_metrics.json': 'CTRL_FLOW',
+                 'rvv_retired_metrics.json': 'RVV',
+                 'rvv_spec_metrics.json': 'RVV',
+                 'tlb_retired_metrics.json': 'TLB',
+                 'tlb_spec_metrics.json': 'TLB',
+                 'topdown_metrics.json': 'TOPDOWN'
                  }
 
 
 def generate_event_adoc(data, out_file, group_name):
     out_file.write('.' + group_name + ' group events\n')
-    out_file.write('[%unbreakable]\n')
+    # out_file.write('[%unbreakable]\n')
     out_file.write('[width="100%",cols="30%,70%",options="header",]\n')
     out_file.write('|===\n')
     out_file.write('|Name |Description\n')
@@ -46,13 +44,13 @@ def generate_event_adoc(data, out_file, group_name):
         desc = event['PublicDescription']
         out_file.write('|' + name + ' |' + desc + '\n')
     out_file.write('|===\n\n')
-    out_file.write('[%unbreakable]\n')
+    # out_file.write('[%unbreakable]\n')
     return
 
 
 def generate_metric_adoc(data, out_file, group_name):
     out_file.write('.' + group_name + ' group metrics\n')
-    out_file.write('[%unbreakable]\n')
+    # out_file.write('[%unbreakable]\n')
     out_file.write('[width="100%",cols="25%,40%,35%",options="header",]\n')
     out_file.write('|===\n')
     out_file.write('|Name |Description |Formula\n')
@@ -62,7 +60,7 @@ def generate_metric_adoc(data, out_file, group_name):
         formula = metric['MetricExpr']
         out_file.write('|' + name + ' |' + desc + ' |' + formula + '\n')
     out_file.write('|===\n\n')
-    out_file.write('[%unbreakable]\n')
+    # out_file.write('[%unbreakable]\n')
     return
 
 
@@ -71,14 +69,14 @@ def generate_adocs():
         with open('event_files/' + event_file) as fr:
             data = json.load(fr)
             adoc_file = event_file.replace('.json', '.adoc')
-            with open('gen/' + adoc_file, "w+") as fw:
+            with open('adoc_event_tables/' + adoc_file, "w+") as fw:
                 generate_event_adoc(data, fw, file_to_group[event_file])
 
     for metric_file in metric_files:
         with open('event_files/' + metric_file) as fr:
             data = json.load(fr)
             adoc_file = metric_file.replace('.json', '.adoc')
-            with open('gen/' + adoc_file, "w+") as fw:
+            with open('adoc_event_tables/' + adoc_file, "w+") as fw:
                 generate_metric_adoc(data, fw, file_to_group[metric_file])
     return
 
